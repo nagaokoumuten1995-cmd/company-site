c80fbb5b6b95e91627213e67e83e041421e224b7
nagaokoumuten1995-cmd <nagao.koumuten1995@gmail.com>
2026-08-05 03:56:56 +0900

    script.js を更新

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("scroll", () => {
+
+    const header = document.querySelector(".site-header");
+
+    if(window.scrollY > 60){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
